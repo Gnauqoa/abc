e701ea95ebaa77604df3ba8a569844fb6c808286
Minh Ha <minhha@aitt.vn>
2023-03-21 15:04:28 +0700

    Add sensor types list

---
+++ src/services/sensor-service.js
@@ -0,0 +1,74 @@
+const sensors = [
+  {
+    id: 0, 
+    name: "Thời gian", 
+    data: {
+      0: {name: "Thời gian", unit: "ms"}, 
+    }
+  },
+  {
+    id: 1, 
+    name: "Cảm biến nồng độ khí C02", 
+    data: {
+      0: {name: "Nồng độ khí CO2", unit: "ppm"}, 
+    }
+  },
+  {
+    id: 2, 
+    name: "Cảm biến Oxi hòa tan trong nước", 
+    data: {
+      0: {name: "DO", unit: "mg/L"}, 
+    }
+  },
+  {
+    id: 3, 
+    name: "Cảm biến nồng độ Oxi trong không khí", 
+    data: {
+      0: {name: "Nồng độ Oxi không khí", unit: "%"}, 
+    }
+  },
+  {
+    id: 4, 
+    name: "Cảm biến nhiệt độ DS18B20", 
+    data: {
+      0: {name: "Nhiệt độ", unit: "°C"}, 
+    }
+  },
+  {
+    id: 5, 
+    name: "Cảm biến nhiệt độ và độ ẩm SHT30", 
+    data: {
+      0: {name: "Nhiệt độ", unit: "°C"}, 
+      1: {name: "Độ ẩm", unit: "%"}, 
+    }
+  },
+  {
+    id: 6, 
+    name: "Cảm biến độ mặn nước", 
+    data: {
+      0: {name: "Độ mặn", unit: "ppt"}, 
+      1: {name: "Độ dẫn điện", unit: "ms/cm"}, 
+    }
+  },
+  {
+    id: 7, 
+    name: "Cảm biến PH", 
+    data: {
+      0: {name: "Độ PH", unit: ""},
+    }
+  },
+  {
+    id: 8, 
+    name: "Cảm biến cường độ âm thanh", 
+    data: {
+      0: {name: "Cường độ âm thanh", unit: "dBA"},
+    }
+  },
+  {
+    id: 9, 
+    name: "Cảm biến áp suất khí", 
+    data: {
+      0: {name: "Áp suất khí", unit: "kPa"},
+    }
+  },
+];
