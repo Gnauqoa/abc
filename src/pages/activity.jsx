c6758659463c56618c9782df81af6e26d2f42d72
Phuc Nguyen <nvphuc@tma.com.vn>
2023-03-21 12:12:43 +0700

    Update images

--- src/pages/activity.jsx
+++ src/pages/activity.jsx
@@ -3,27 +3,25 @@ import { Page, Link, Navbar, NavTitle, NavLeft } from "framework7-react";
 
 import activityImg from "../img/activity/activity.png";
 
-export default class extends Component {
-  render() {
-    const layout = this.props.f7route.params.layout;
+export default ({ f7route, f7router }) => {
+  const layout = f7route.params.layout;
 
-    return (
-      <Page className="bg-color-regal-blue custom-dashboards">
-        <Navbar className="custom-dashboards-navbar">
-          <NavLeft>
-            <Link
-              iconIos="material:arrow_back"
-              iconMd="material:arrow_back"
-              className="back-icon"
-              onClick={() => this.props.f7router.back()}
-            />
-          </NavLeft>
-          <NavTitle>Layout {layout}</NavTitle>
-        </Navbar>
-        <div className="page-content display-flex justify-content-center align-items-center">
-          <img src={activityImg} className="responsive" />
-        </div>
-      </Page>
-    );
-  }
-}
+  return (
+    <Page className="bg-color-regal-blue custom-dashboards">
+      <Navbar className="custom-dashboards-navbar">
+        <NavLeft>
+          <Link
+            iconIos="material:arrow_back"
+            iconMd="material:arrow_back"
+            className="back-icon"
+            onClick={() => f7router.back()}
+          />
+        </NavLeft>
+        <NavTitle>Layout {layout}</NavTitle>
+      </Navbar>
+      <div className="page-content display-flex justify-content-center align-items-center">
+        <img src={activityImg} className="responsive" />
+      </div>
+    </Page>
+  );
+};
