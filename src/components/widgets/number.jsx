95a554cdf8c52c83c562ec666c13aee6e796c864
Phuc Nguyen <phuclx@gmail.com>
2023-03-27 09:42:25 +0700

    Update number widget

--- src/components/widgets/number.jsx
+++ src/components/widgets/number.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { getUnit } from "../../services/sensor-service";
 import "./number.scss";
 
-export default ({ value }) => {
-  return <div className="number-widget">{value}</div>;
+export default ({ value, widget }) => {
+  const sensor = widget.sensor;
+  return (
+    <div className="number-widget">
+      {value} <span>{value.length && getUnit(sensor.id, sensor.index)}</span>
+    </div>
+  );
 };
