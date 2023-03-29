e0dd46d12168483cf339b5aaaee45cbd3b591319
Vuong Huynh <hoangvuong19991964@gmail.com>
2023-03-29 22:43:51 +0700

    enhanched number selector

--- src/components/widgets/line_chart.jsx
+++ src/components/widgets/line_chart.jsx
@@ -5,7 +5,7 @@ import zoomPlugin from "chartjs-plugin-zoom";
 
 Chart.register(zoomPlugin);
 
-import SensorSelector from "../sensor-selector";
+import SensorSelectorEnhanched from "../sensor-selector-enhanched";
 
 const log = (text, data) => {
   let debug = true;
@@ -180,13 +180,14 @@ const updateChart = ({ chartInstance, data, xUnit, yUnit, maxHz }) => {
   let suggestedMaxX = calculateSuggestMaxX({
       chartData: data,
       pageStep,
-    }), stepSize;
-  
-  if(!suggestedMaxX) {
+    }),
+    stepSize;
+
+  if (!suggestedMaxX) {
     suggestedMaxX = pageStep;
   }
 
-  stepSize = Math.round(suggestedMaxX / 10)
+  stepSize = Math.round(suggestedMaxX / 10);
 
   chartInstance.data = createChartJsData({
     chartData: data,
@@ -205,7 +206,7 @@ const updateChart = ({ chartInstance, data, xUnit, yUnit, maxHz }) => {
     },
     x: {
       type: "linear",
-      suggestedMin : 0,
+      suggestedMin: 0,
       suggestedMax: suggestedMaxX,
       ticks: {
         // forces step size to be 50 units
@@ -272,7 +273,7 @@ let LineChart = (props, ref) => {
     }),
     lastPositionOnChart = useRef({
       x: 0,
-      y: 0
+      y: 0,
     }),
     valueContainerElRef = useRef(),
     xElRef = useRef(),
@@ -393,7 +394,7 @@ let LineChart = (props, ref) => {
       chartInstance: chartInstanceRef.current,
       data: [],
       xUnit: "ms",
-      yUnit: ""
+      yUnit: "",
     });
 
     // return () => {
@@ -405,16 +406,20 @@ let LineChart = (props, ref) => {
   return (
     <div className="line-chart-wapper">
       <div className="sensor-select-container">
-        <SensorSelector
+        <SensorSelectorEnhanched
           selectedSensor={widget.sensor}
           onChange={(sensor) => handleSensorChange(widget.id, sensor)}
-        ></SensorSelector>
+        ></SensorSelectorEnhanched>
       </div>
       <div className="canvas-container">
         <canvas ref={chartEl} />
         <div className="current-value-sec" ref={valueContainerElRef}>
-          <div className="value-container">x = &nbsp;<span ref={xElRef}></span></div>
-          <div className="value-container">y = &nbsp;<span ref={yElRef}></span></div>
+          <div className="value-container">
+            x = &nbsp;<span ref={xElRef}></span>
+          </div>
+          <div className="value-container">
+            y = &nbsp;<span ref={yElRef}></span>
+          </div>
         </div>
       </div>
     </div>
--- src/components/widgets/number.jsx
+++ src/components/widgets/number.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { getUnit } from "../../services/sensor-service";
 import SensorSelector from "../sensor-selector";
 import "./number.scss";
+import SensorSelectorEnhanched from "../sensor-selector-enhanched";
 
 export default ({ value, widget, handleSensorChange }) => {
   const sensor = widget.sensor;
@@ -11,10 +12,10 @@ export default ({ value, widget, handleSensorChange }) => {
         {value} <span className="__unit">{value.length && getUnit(sensor.id, sensor.index)}</span>
       </div>
       <div className="sensor-select-container">
-        <SensorSelector
+        <SensorSelectorEnhanched
           selectedSensor={widget.sensor}
           onChange={(sensor) => handleSensorChange(widget.id, sensor)}
-        ></SensorSelector>
+        ></SensorSelectorEnhanched>
       </div>
     </div>
   );
