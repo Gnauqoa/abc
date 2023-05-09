8231768523b3c1f957630e01243b999da4ef4365
KhoaTLV <khoa.truong1799@gmail.com>
2023-05-09 15:48:13 +0700

    Add Navigate and Change Page Name

---
+++ src/components/atoms/popover-page-management/index.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Popover, List, Button, ListItem, f7 } from "framework7-react";
+
+import "./index.scss";
+import { useActivityContext } from "../../../context/ActivityContext";
+import usePrompt from "../../../hooks/useModal";
+
+const PageManagement = () => {
+  const { pages, changePageName, currentPageIndex, handleNavigatePage } = useActivityContext();
+
+  const onCloseChangeNamePopup = (newPageName) => {
+    newPageName && changePageName(currentPageIndex, newPageName);
+  };
+
+  const { prompt, showModal } = usePrompt({
+    title: "Đổi tên trang hiện tại",
+    defaultValue: pages[currentPageIndex].name,
+    onClosePopup: onCloseChangeNamePopup,
+  });
+
+  const handlePageChangeName = () => {
+    f7.popover.close();
+    showModal();
+  };
+
+  const handleChangePage = (pageIndex) => {
+    handleNavigatePage(pageIndex);
+    f7.popover.close();
+  };
+
+  return (
+    <div>
+      <Popover className="popover-page-management">
+        <List>
+          <ListItem className="rename-page-button" key="rename-page-button" onClick={handlePageChangeName}>
+            Đổi tên trang hiện tại
+          </ListItem>
+          <ListItem>Danh sách các trang</ListItem>
+          <List>
+            {pages.map((page, index) => (
+              <Button
+                className={`page-name ${index === currentPageIndex && "selected"}`}
+                onClick={() => handleChangePage(index)}
+              >
+                {page.name}
+              </Button>
+            ))}
+          </List>
+        </List>
+      </Popover>
+      {prompt}
+    </div>
+  );
+};
+
+export default PageManagement;
