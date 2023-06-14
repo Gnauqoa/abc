89fda073e8aed8b4d2d89ed02546102549f0b31e
Minh Ha <minhha@aitt.vn>
2023-06-14 12:27:08 +0700

    Rename sensor

--- src/services/sensor-service.js
+++ src/services/sensor-service.js
@@ -74,7 +74,7 @@ export const defaultSensors = [
     id: 5,
     code: "inno-005",
     name: "Cảm biến độ ẩm không khí",
-    label: "Nhiệt độ Độ ẩm",
+    label: "Độ ẩm Nhiệt độ",
     icon: tempHumiditySensorIcon,
     data: [
       { id: "inno-005-hum", name: "Độ ẩm", unit: "%", min: 0, max: 100, formatFloatingPoint: 0 },
